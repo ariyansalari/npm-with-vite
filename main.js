dd98c5346fb64220c13a92a9362cf9c1cd5892de
ariyan Salari <ariyan4509@gmail.com>
2023-02-13 02:50:30 -0800

    training with npm

---
+++ main.js
@@ -0,0 +1,5 @@
+import App from "./src/App";
+
+const root=document.getElementById("app");
+
+root.innerHTML=App();
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,6 @@
+// import styles from 
+const Navbar=()=>{
+    return (`<h1 class="hi">ariyan hastam</h1>`)
+}
+
+export default Navbar;
